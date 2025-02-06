f7991b04a44ed30355e936fff96d9ad5fe77c5b0
Ryan <lizkaryan626@gmail.com>
2025-02-07 01:09:09 +0300

    login admin

--- public/app-angular/login.js
+++ public/app-angular/login.js
@@ -2,11 +2,15 @@ let loginApp = angular.module("loginApp",["ngRoute"]);
 
 loginApp.controller('loginController', function($scope, $http) {
     $scope.utilisateur={};
+    $scope.utilisateur.email="LizkaRyan626@gmail.com";
+    $scope.utilisateur.password="itu16";
+
     const buttonHtml="\n" +
         "            <span class=\"spinner-border spinner-border-sm\" role=\"status\" aria-hidden=\"true\"></span>\n" +
         "            <span class=\"visually-hidden\">Loading...</span>";
     const buttonLogin=document.getElementById("buttonLogin");
     const message=document.getElementById("message");
+
     $scope.submitForm=function(){
         console.log(JSON.stringify($scope.utilisateur));
         buttonLogin.innerHTML=buttonHtml;
@@ -20,8 +24,13 @@ loginApp.controller('loginController', function($scope, $http) {
             console.log($scope.data);
             buttonLogin.innerHTML="Login";
             buttonLogin.disabled=false;
+            console.log($scope.data);
             if($scope.data.status==200){
-                window.location.href = `http://127.0.0.1:8000/pin?token=${$scope.data.data}`;
+                url=`http://127.0.0.1:8000/pin?token=${$scope.data.data.token}`;
+                if($scope.data.data.isAdmin){
+                    url = `http://127.0.0.1:8000/session?idUtilisateur=${$scope.data.data.utilisateur.idUtilisateur}&pseudo=${$scope.data.data.utilisateur.utilisateur}&role=${$scope.data.data.utilisateur.role.roleName}&token=${$scope.data.data.token}`;
+                }
+                window.location.href=url;
             }
             else{
                 message.innerHTML="<div class=\"alert alert-danger\" role=\"alert\">\n" +
